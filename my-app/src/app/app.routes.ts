1b3d0201d81d798ad9ae468b53367da1f017d7f5
Shubham <shubhambhanarkar.janbask@gmail.com>
2025-07-10 18:38:28 +0530

    data pass 1

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { About } from './about/about';
 import { Contact } from './contact/contact';
 import { Home } from './home/home';
 import { PageNotFound } from './page-not-found/page-not-found';
+import { Profile } from './profile/profile';
 
 export const routes: Routes = [
     { path: "", component: Home },
     { path: "login", component: Login },
     { path: "about", component: About },
     { path: "contact", component: Contact },
+    { path: "profile", component: Profile },
     { path: '**', component: PageNotFound }
 
 ];
