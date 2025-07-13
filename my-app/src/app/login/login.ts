3816a900b03222873d376bfd54375461bc79837b
Shubham <shubhambhanarkar4786@gmail.com>
2025-07-14 03:32:58 +0530

    Validation

--- my-app/src/app/login/login.ts
+++ my-app/src/app/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -11,14 +11,24 @@ export class Login {
   profileForm = new FormGroup(
     {
 
-      name: new FormControl(),
-      password: new FormControl(),
-      email: new FormControl()
+      name: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      email: new FormControl('', [Validators.required, Validators.maxLength(50), Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$')])
     }
   )
 
   handleSubmit() {
     console.log(this.profileForm.value);
 
+  }
+
+  get name() {
+    return this.profileForm.get('name')
+  }
+  get password() {
+    return this.profileForm.get('password')
+  }
+  get email() {
+    return this.profileForm.get('email')
   }
 }
