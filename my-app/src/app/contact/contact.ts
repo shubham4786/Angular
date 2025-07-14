774dfe08cb5e21ff28eb83e9fc06ec76bdf963a9
Shubham <shubhambhanarkar4786@gmail.com>
2025-07-14 06:01:08 +0530

    Angular Pipes

--- my-app/src/app/contact/contact.ts
+++ my-app/src/app/contact/contact.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css'
 })
 export class Contact {
+  title = 'contact component'
+  name = 'SHUBHAM'
+  date = new Date()
 
+  currency = 10
 }
