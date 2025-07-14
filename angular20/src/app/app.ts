c5e2bbe4184f222e898465ecd4cb24dea4597ada
Shubham <shubhambhanarkar4786@gmail.com>
2025-07-14 20:50:18 +0530

    angular 20

---
+++ angular20/src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+
+}
